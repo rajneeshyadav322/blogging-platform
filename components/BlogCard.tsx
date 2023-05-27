678dc3a0c507eb14c8b2d6ec3f51416e692b400b
rajneeshAntino <rajneesh.y@antino.io>
2023-05-27 18:05:19 +0530

    added

--- components/BlogCard.tsx
+++ components/BlogCard.tsx
@@ -18,14 +18,17 @@ const BlogCard = (props: PropTypes) => {
       <div className="p-0" onClick={() => router.push(`blog/${blog.id}`)}>
         <div className="flex gap-8 mt-8 w-full">
           <div className="relative w-[250px]">
-            <Image alt="Banner Image" src={blog.bannerImage} fill />
+            {blog?.bannerImage && <Image alt="Banner Image" src={blog.bannerImage  } fill />}
           </div>
           <div className="w-full cursor-pointer">
             <div className="text-2xl font-bold">{blog.title}</div>
-            <div
+            {
+              blog?.blog &&
+              <div
               className="font-light h-12 text-ellipsis overflow-hidden"
               dangerouslySetInnerHTML={{ __html: blog.blog }}
-            ></div>
+              ></div>
+            }
             <div className="flex w-full justify-between mt-2">
               <div className="py-[2px] px-4 text-center bg-[#f3ebeb] rounded-md text-sm">
                 {blog.category}
